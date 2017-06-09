b81591f7f608b0b41805bd94a61e76139e83a97a
mdelamare <mederic.delamare@trsb.net>
2017-06-09 18:45:27 +0200

    feat(stats):  calendrier

--- src/AppBundle/Controller/StatsAController.php
+++ src/AppBundle/Controller/StatsAController.php
@@ -21,12 +21,56 @@ class StatsAController extends Controller
 
         $resultats = $this->getResults('http://eure.fff.fr/competitions/php/championnat/championnat_resultat.php?sa_no=2016&cp_no=328937&ph_no=1&gp_no=1');
         $classement = $this->getClassement('http://eure.fff.fr/competitions/php/championnat/championnat_classement.php?sa_no=2016&cp_no=328937&ph_no=1&gp_no=1');
+        $calendrier = $this->getCalendrier('http://eure.fff.fr/competitions/php/championnat/championnat_calendrier_resultat.php?cp_no=328937&ph_no=1&gp_no=1&sa_no=2016&typ_rech=equipe&cl_no=104246&eq_no=1&type_match=deux&lieu_match=deux');
 
-        dump($classement);
         return $this->render(':default:statistiqueA.html.twig', [
             'resultats' => $resultats,
-            'classement' => $classement
+            'classement' => $classement,
+            'calendrier' => $calendrier
         ]);
+    }
+
+    private function getCalendrier($html){
+        $ch = curl_init();
+
+
+        curl_setopt($ch, CURLOPT_URL, $html);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_NOBODY, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, $_SERVER['REQUEST_URI']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_exec($ch);
+
+        $html = curl_exec($ch);
+
+        curl_close($ch);
+
+        $crawler = new Crawler($html);
+
+        $crawler->filter('.resultatmatch');
+
+        $resultats = [];
+
+        for ($i = 0; $i < $crawler->filter('.resultatmatch')->count(); $i++) {
+            $craw = $crawler->filter('.resultatmatch')->eq($i);
+            $equipe1 = $craw->filter('.matchLink')->eq(0)->text();
+            $equipe2 = $craw->filter('.matchLink')->eq(1)->text();
+            $score = $craw->filter('.score')->first()->text();
+            $resultats[] = [
+                'equipe1' => $equipe1,
+                'equipe2' => $equipe2,
+                'score' => $score
+            ];
+        }
+
+        return $resultats;
     }
 
     private function getClassement($html){
