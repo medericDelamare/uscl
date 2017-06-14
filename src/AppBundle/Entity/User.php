e7c7180b17fb5de7f8a8141a65545e19a1576934
mdelamare <mederic.delamare@trsb.net>
2017-06-14 13:34:28 +0200

    feat(fos): changements de templates

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,9 +2,25 @@
 
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Model\User
 
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
 class User extends BaseUser
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
 }
