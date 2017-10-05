d1bcd5e487bf47ab0cddc3bfcd8b891545ebb759
mdelamare <mederic.delamare@trsb.net>
2017-10-05 14:17:00 +0200

    feat(index): récupération des réslutats du clubs

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,11 +15,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $prochainsMatchs = $this->getAgenda('https://www.fff.fr/la-vie-des-clubs/104246/agenda');
+        $resultats = $this->getResult('https://eure.fff.fr/recherche-clubs/?scl=104246&tab=resultats');
+
+        dump($resultats);
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'prochainsMatchs' => $prochainsMatchs
+            'prochainsMatchs' => $prochainsMatchs,
+            'resultats' => $resultats
         ]);
     }
     private function getAgenda($html){
@@ -78,8 +82,75 @@ class DefaultController extends Controller
         return $resultats;
     }
 
+    private function getResult($url){
+        $ch = curl_init();
+
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_NOBODY, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, $_SERVER['REQUEST_URI']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_exec($ch);
+
+        $html = curl_exec($ch);
+
+        curl_close($ch);
+
+        $crawler = new Crawler($html);
+
+
+
+
+        $crawler->filter('.confrontation');
+
+
+        for ($i = 0; $i < $crawler->filter('.confrontation')->count(); $i++) {
+            $craw = $crawler->filter('.confrontation')->eq($i);
+            $competition = $craw->filter('.competition')->text();
+            $equipe1 = trim($craw->filter('.equipe1')->filter('.name')->text());
+            $equipe2 = trim($craw->filter('.equipe2')->text());
+
+            if (($craw->filter('.number')->count() > 0)) {
+                $score = $craw->filter('.score_match')->html();
+                $scoreExposed = explode(' - ', $score);
+                $scoreDomicile = $this->getNombre($scoreExposed[0]);
+                $scoreExterieur = $this->getNombre($scoreExposed[1]);
+                $score = $scoreDomicile . ' - ' . $scoreExterieur;
+            } else {
+                $score = '-';
+            }
+            $resultats[] = [
+                'equipe1' => $equipe1,
+                'equipe2' => $equipe2,
+                'competition' => $competition,
+                'score' => $score
+            ];
+        }
+
+        $crawler->filter('.date span')->each(function (Crawler $crawler) {
+            foreach ($crawler as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        });
+
+        for ($i = 0; $i < $crawler->filter('.date')->count(); $i++) {
+            $craw = $crawler->filter('.date')->eq($i);
+            $resultats[$i]['date'] = $this->convertDate(str_replace('<br>', '',$craw->html()));
+        }
+
+        return $resultats;
+    }
+
     public function convertDate($date){
-        $dateRegex = preg_replace('(\s+)', ' ', $date);
+        $dateRegex = trim(preg_replace('(\s+)', ' ', $date));
         $dateSplit = explode(' ',$dateRegex);
 
         switch (strtolower($dateSplit[2])){
@@ -124,5 +195,58 @@ class DefaultController extends Controller
                 break;
         }
         return $dateSplit[1] . "/" . $dateSplit[2] . "/" . $dateSplit[3];
+    }
+
+    public function getNumberByImgUrl($imgUrl)
+    {
+        if ($imgUrl == 'vide') {
+            return '';
+        }
+        $imgHeaders = get_headers($imgUrl, 1);
+
+        switch ($imgHeaders['Content-Length']) {
+            case '2543':
+                $score = '0';
+                break;
+            case '733':
+                $score = '1';
+                break;
+            case '1921':
+                $score = '2';
+                break;
+            case '1987':
+                $score = '3';
+                break;
+            case '1595':
+                $score = '4';
+                break;
+            case '1909':
+                $score = '5';
+                break;
+            case '2632':
+                $score = '6';
+                break;
+            case '1276':
+                $score = '7';
+                break;
+            case '2585':
+                $score = '8';
+                break;
+            case '2570':
+                $score = '9';
+                break;
+        }
+
+        return $score;
+    }
+
+    private function getNombre($html)
+    {
+        $crawler = new Crawler($html);
+        $nombre = null;
+        for ($i = 0; $i <= $crawler->filter('.number')->count() - 1; $i++) {
+            $nombre = $nombre . $this->getNumberByImgUrl($crawler->filter('.number')->eq($i)->attr('src'));
+        }
+        return $nombre;
     }
 }
