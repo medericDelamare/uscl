446099ca7bfcdfdb4f16b52a2e65ceab16c5b5dc
mdelamare <mederic.delamare@trsb.net>
2017-10-12 14:28:37 +0200

    feat(admin): ajout formulaire stats

--- src/AppBundle/Controller/StatsAdminController.php
+++ src/AppBundle/Controller/StatsAdminController.php
@@ -2,10 +2,39 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Joueur;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StatsAdminController extends Controller{
     public function addAction(){
-        return $this->render('@SonataAdmin/Stats/stats.html.twig');
+
+        $joueursBdd = $this->getDoctrine()->getManager()->getRepository(Joueur::class)->findAll();
+
+        /** @var Joueur $joueur */
+        foreach ($joueursBdd as $joueur){
+            $joueurs[$joueur->getNom() . ' - ' . $joueur->getPrenom()] = $joueur;
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('passeurs', ChoiceType::class, [
+                'choices' => $joueurs,
+                'multiple' => true
+            ])
+            ->add('buteurs', ChoiceType::class, [
+                'choices' => $joueurs,
+                'multiple' => true
+            ])
+            ->add('nbMatchs', ChoiceType::class, [
+                'choices' => $joueurs,
+                'multiple' => true
+            ])
+            ->add('Envoyer', SubmitType::class)
+            ->getForm();
+        return $this->render('@SonataAdmin/Stats/stats.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
