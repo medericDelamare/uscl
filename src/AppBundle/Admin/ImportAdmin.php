a8e51bdadef0711a0b6cf9f09a29e36348c768b4
mdelamare <mederic.delamare@trsb.net>
2018-04-28 16:40:31 +0200

    feat(import-joueurs): Création de la commande et de l'admin

--- src/AppBundle/Admin/ImportAdmin.php
+++ src/AppBundle/Admin/ImportAdmin.php
@@ -5,15 +5,49 @@ namespace AppBundle\Admin;
 
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
 class ImportAdmin extends AbstractAdmin
 {
-    protected $baseRouteName = 'import';
-    protected $baseRoutePattern = 'import';
+    protected $baseRouteName = 'imports';
+    protected $baseRoutePattern = 'imports';
+
+    public function getDashboardActions()
+    {
+        $actions = parent::getDashboardActions();
+
+        $actions['import'] = array(
+            'label'              => 'Ajouter un import',
+            'url'                => $this->generateUrl('add'),
+            'icon'               => 'import',
+            'translation_domain' => 'SonataAdminBundle', // optional
+            'template'           => 'SonataAdminBundle:CRUD:dashboard__action.html.twig', // optional
+        );
+
+        return $actions;
+    }
 
     protected function configureRoutes(RouteCollection $collection)
     {
-        $collection->add('import','import/{fileName}');
+        $collection->add('add');
+        $collection->remove('list');
+        $collection->remove('create');
+        $collection->remove('edit');
+        $collection->remove('show');
+        $collection->remove('batch');
+        $collection->remove('delete');
+        $collection->remove('export');
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+
     }
 }
--- src/AppBundle/Controller/ImportAdminController.php
+++ src/AppBundle/Controller/ImportAdminController.php
@@ -4,8 +4,56 @@
 namespace AppBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ImportAdminController extends Controller
 {
+    /**
+     * @Route("/add", name="add-import")
+     */
+    public function addAction(){
 
+        /** @var Kernel $kernel */
+        $kernel = $this->container->get( 'kernel' );
+        $dir = $kernel->getRootDir() . '\Resources\Infos_Footclub';
+        $request = $this->getRequest();
+        /** @var Form $form */
+        $form = $this->createFormBuilder()
+            ->add('file', FileType::class)
+            ->add('Envoyer', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+
+        $finder = new Finder();
+        $files = $finder->in($dir)->files()->name('*.csv')->sort(function ($a, $b) { return strcmp($b->getRealpath(), $a->getRealpath()); });
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $file */
+            $file = $form['file']->getData();
+            if ($file->getClientOriginalExtension() == 'csv'){
+                $name = $file->getClientOriginalName();
+                if (file_exists($dir . '/' . $name)){
+                    $this->addFlash('sonata_flash_error', 'Le fichier existe déjà dans le repertoire');
+                    return $this->redirect($request->getRequestUri());
+                }
+                $file->move($dir, $name);
+
+            }
+            else{
+                $this->addFlash('sonata_flash_error', 'Le format du fichier choisi n\'est pas du type CSV');
+                return $this->redirect($request->getRequestUri());
+            }
+        }
+        return $this->render('@SonataAdmin/Import/import.html.twig', [
+            'files' => $files,
+            'form'=> $form->createView(),
+        ]);
+    }
 }
