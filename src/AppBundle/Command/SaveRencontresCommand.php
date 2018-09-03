ae85cd8b8c61b0e0126ccbcf4e5d16511bf6e9c9
Unknown <delamare.mederic@gmail.com>
2018-09-03 19:27:41 +0200

    feat(commande): externalisation de l'url et de la categorie

--- src/AppBundle/Command/SaveRencontresCommand.php
+++ src/AppBundle/Command/SaveRencontresCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 class SaveRencontresCommand extends ContainerAwareCommand
@@ -9,14 +10,19 @@ class SaveRencontresCommand extends ContainerAwareCommand
     {
         $this
             ->setName('save:rencontres')
-            ->setDescription('Récupération des données sur le disctrict');
+            ->setDescription('Récupération des données sur le disctrict')
+            ->addArgument('url', InputArgument::REQUIRED, 'Url?')
+            ->addArgument('categorie', InputArgument::REQUIRED, 'Categorie?')
+        ;
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $url = $input->getArgument('url');
+        $categorie = $input->getArgument('categorie');
         $now = new \DateTime();
         $output->writeln('<comment>Start : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
         $parseService = $this->getContainer()->get('parse.parse_scores');
-        $parseService->getStats('https://eure.fff.fr/competitions/?id=351217&poule=1&phase=1&type=ch&tab=calendar', 'seniorA');
+        $parseService->getStats($url, $categorie);
         $now = new \DateTime();
         $output->writeln('<comment>End : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
     }
