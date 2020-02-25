dd00cc4bc80cb4846e765a4e1af01cd630236972
mdelamare <delamare.mederic@gmail.com>
2020-02-25 23:10:09 +0100

    feat(service): Utilisation de l'id scorenco

--- src/AppBundle/Service/StatistiquesService.php
+++ src/AppBundle/Service/StatistiquesService.php
@@ -5,10 +5,19 @@ namespace AppBundle\Service;
 
 
 use AppBundle\Entity\Licencie;
+use AppBundle\Entity\StatsRencontre;
 use Doctrine\Common\Collections\Criteria;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class StatistiquesService
 {
+    private $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
     public function getLastFiveResultByPlayer(Licencie $licencie){
         $statsRencontres = $licencie->getStatsRencontresJoueurs();
 
@@ -46,17 +55,19 @@ class StatistiquesService
         $n=0;
         $d=0;
 
+        /** @var StatsRencontre $stats */
         foreach ($licencie->getStatsRencontresJoueurs() as $stats) {
-            if ($stats->getRencontre()->getScoreDom() != null && $stats->getRencontre()->getScoreDom() == $stats->getRencontre()->getScoreExt()){
+            $scoreDom = $stats->getRencontre()->getScoreDom();
+            $scoreExt = $stats->getRencontre()->getScoreExt();
+            $usclScorencoParameter = $this->kernel->getContainer()->getParameter('uscl_scorenco_id');
+            
+            if ($scoreDom != null && $scoreDom == $scoreExt){
                 $n++;
-            }
-            else if ($stats->getRencontre()->getEquipeDomicile()->getClub()->getNom() == "USCL" && $stats->getRencontre()->getScoreDom() > $stats->getRencontre()->getScoreExt()){
+            } else if ($stats->getRencontre()->getEquipeDomicile()->getClub()->getScorencoId() == $usclScorencoParameter && $scoreDom > $scoreExt){
                 $v++;
-            }
-            else if ($stats->getRencontre()->getEquipeExterieure()->getClub()->getNom() == "USCL" && $stats->getRencontre()->getScoreDom() < $stats->getRencontre()->getScoreExt()){
+            } else if ($stats->getRencontre()->getEquipeExterieure()->getClub()->getScorencoId() == $usclScorencoParameter && $scoreDom < $scoreExt){
                 $v++;
-            }
-            else{
+            } else{
                 $d++;
             }
         }
