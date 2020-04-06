37a68d1f42efa0350d8ca0aba54c0e4c1918dccd
mdelamare <delamare.mederic@gmail.com>
2020-04-06 15:34:23 +0200

    feat(bureau): Refonte de la partie desktop de la page bureau

--- assets/js/base.js
+++ assets/js/base.js
@@ -11,6 +11,6 @@ $(document).ready(function(){
         });
     }
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="tooltip"]').tooltip();
     });
 });
