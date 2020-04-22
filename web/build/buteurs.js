dc3fe0f2de93dc45660d0193977e7887efe206b8
mdelamare <delamare.mederic@gmail.com>
2020-04-22 15:42:23 +0200

    feat(css-js): Minification

--- web/build/buteurs.js
+++ web/build/buteurs.js
@@ -1,10 +1,1 @@
-$('.tableau-buteur').DataTable({
-  "order": [[1, "desc"]],
-  "language": {
-    "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
-  },
-  "autoWidth": false,
-  "paging": false,
-  "bInfo": false,
-  "searching": false
-});
+(window.webpackJsonp=window.webpackJsonp||[]).push([["buteurs"],{a2mw:function(a,n){$("http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js",(function(){$(".tableauButeur").DataTable({order:[[1,"desc"]],language:{url:"//cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"},autoWidth:!1,paging:!1,bInfo:!1,searching:!1})}))}},[["a2mw","runtime"]]]);
